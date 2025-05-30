af485e0ca5e3c440bc7201d4c12ef1f5fd764177
Alondra Espinoza <alondraespinoza897@gmail.com>
2025-05-30 17:34:39 -0600

    Creacion del AuthController

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiProyect.Services;
+using WebApiProyect.Models;
+
+namespace WebApiProyect.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly AuthenticationService _authService = new();
+
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] Usuario usuario)
+    {
+        bool isValid = _authService.ValidateUser(usuario.Username, usuario.Password);
+
+        if (!isValid)
+            return Unauthorized("Credenciales inválidas");
+
+      
+        return Ok("Login exitoso");
+    }
+}
